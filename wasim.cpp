5c0dce396b8a1335b27257095ab9bf091b93928c
Wasim <wasim.shaikh@cctech.co.in>
2022-03-03 01:06:40 +0530

    ignore file added

--- wasim.cpp
+++ wasim.cpp
@@ -7,5 +7,12 @@ int main(){
     cout<<"gitbash practice wasim branch";
     cout<<"gitbash practice wasim branch";
     cout<<"gitbash practice wasim branch";
+    cout<<"gitbash practice wasim branch";
+    cout<<"gitbash practice wasim branch";
+    cout<<"gitbash practice wasim branch";
+    cout<<"gitbash practice wasim branch";
+    cout<<"gitbash practice wasim branch";
+    cout<<"gitbash practice wasim branch";
+    cout<<"gitbash practice wasim branch";
     return 0;
 }
